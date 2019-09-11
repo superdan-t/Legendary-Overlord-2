7a16f1e0f0b3a0e4214412324c46bdaffdeac501
Dan T <cat101126@gmail.com>
2019-09-10 23:07:43 -0400

    Serial input now supported

--- LEGO3/LEGO3.ino
+++ LEGO3/LEGO3.ino
@@ -4,13 +4,52 @@
 #define VER_MINOR 0
 #define VER_PATCH 0
 
-Dimmer dimmers[256];
+#define EOT 4
+#define DLE 16
+
+Dimmer dimmers[51];
+
+byte serialBuf[50];
+byte serialIndex = 0;
 
 void setup() {
+
+  Serial.begin(115200);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    byte in = Serial.read();
+    if (in != EOT && in != DLE) {
+      //Normal char received
+      serialBuf[serialIndex] = in;
+      serialIndex++;
+    } else if (in == DLE) {
+      //Escape char received
+      serialBuf[serialIndex] = DLE;
+    } else if (serialBuf[serialIndex] == DLE) {
+      //Last char was escaped. in can only equal 4 or 16 at this point
+      serialBuf[serialIndex] = in;
+      serialIndex++;
+    } else if (in == EOT) {
+      //End of transmission
+      byte replyBuf[50];
+      byte replySize;
+      if (interpret(serialBuf, replyBuf, &replySize, 50)) {
+        for (byte i = 0; i < replySize; i++) {
+          if (replyBuf[i] == EOT || replyBuf[i] == DLE) {
+            Serial.write(DLE);
+          }
+          Serial.write(replyBuf[i]);
+        }
+        Serial.write(EOT);
+      }
+      
+    }
+  }
 }
